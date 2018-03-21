027a21559855b01f1180580024fc92e9e16539a2
EdgarSG <EdgarSG@EdgarSG-PC>
2018-03-21 17:59:22 -0500

    Primera Version del programa

--- Documents/Programacion/ButtonGroup/src/AgruparBotones.java
+++ Documents/Programacion/ButtonGroup/src/AgruparBotones.java
@@ -1,4 +1,128 @@
+/*
+***********************************************
+Proyecto: AgruparBotones
+Autor: Edgar Antonio Salas González
+Ultima edición: 25 de febrero del 2018
 
-public class AgruparBotones {
+***********************************************
+*/
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class AgruparBotones extends JFrame
+        implements ActionListener {
+
+    public AgruparBotones(String titulo) {
+        //Creación de los RadioButtons
+        JRadioButton birdButton = new JRadioButton("Pajarito");
+        birdButton.setMnemonic(KeyEvent.VK_B);
+        birdButton.setActionCommand("bird");
+        birdButton.setSelected(true);
+
+        JRadioButton catButton = new JRadioButton("Gatito");
+        catButton.setMnemonic(KeyEvent.VK_C);
+        catButton.setActionCommand("cat");
+
+        JRadioButton dogButton = new JRadioButton("Perrito");
+        dogButton.setMnemonic(KeyEvent.VK_D);
+        dogButton.setActionCommand("dog");
+
+        JRadioButton rabbitButton = new JRadioButton("Conejito");
+        rabbitButton.setMnemonic(KeyEvent.VK_R);
+        rabbitButton.setActionCommand("rabbit");
+
+        JRadioButton pigButton = new JRadioButton("Cerdito");
+        pigButton.setMnemonic(KeyEvent.VK_P);
+        pigButton.setActionCommand("pig");
+
+        ButtonGroup btgAnimalitos = new ButtonGroup();
+
+        btgAnimalitos.add(birdButton);
+        btgAnimalitos.add(catButton);
+        btgAnimalitos.add(dogButton);
+        btgAnimalitos.add(rabbitButton);
+        btgAnimalitos.add(pigButton);
+
+        JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new GridLayout(0,1));
+        
+        //radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.Y_AXIS));
+        radioPanel.add(birdButton);
+        radioPanel.add(catButton);
+        radioPanel.add(dogButton);
+        radioPanel.add(rabbitButton);
+        radioPanel.add(pigButton);
+        radioPanel.setBackground(Color.RED);
+
+        JLabel picture = new JLabel(new ImageIcon("src/loro.png"));
+        
+        //Se crea un contenedor Animalitos donde se agrega el radioPanel y la imagen
+        JPanel Animalitos = new JPanel();
+        Animalitos.setLayout(new BoxLayout(Animalitos,BoxLayout.X_AXIS));
+        Animalitos.add(radioPanel);
+        Animalitos.add(picture);
+        getContentPane().add(picture,BorderLayout.CENTER);
+        getContentPane().add(radioPanel,BorderLayout.WEST);
+        getContentPane().setBackground(Color.GREEN);
+
+        //Se crean los dos botones
+        JButton Button1 = new JButton("Botón 1");
+        JButton Button2 = new JButton("Botón 2");
+        
+        //Se crea un contenedor para poner los dos botones 
+        JPanel Botones = new JPanel();
+        Botones.setLayout(new BoxLayout(Botones,BoxLayout.X_AXIS));
+        Botones.add(Button1);
+        Botones.add(Button2);
+        Botones.setBackground(Color.pink);
+
+
+        //Se agragan al panel principal y se posicionan en su lugar
+        JPanel Principal = new JPanel();
+        Principal.setLayout(new BorderLayout());
+        getContentPane().add(Animalitos, BorderLayout.NORTH);
+        getContentPane().add(Botones, BorderLayout.SOUTH);
+        
+        /*BoxLayout distBoxLayout = new BoxLayout(this.getContentPane(),
+                                                BoxLayout.X_AXIS);
+        getContentPane().setLayout(distBoxLayout);
+        */
+         
+        //GridLayout distGrid = new GridLayout(1,2);
+        /*
+        getContentPane().setLayout(distGrid);
+        getContentPane().add(radioPanel);
+        getContentPane().add(picture);
+        */
+
+        /*
+        getContentPane().add(radioPanel, BorderLayout.WEST);
+        getContentPane().add(picture,BorderLayout.CENTER);
+        */
+
+        getContentPane().setBackground(Color.BLUE);
+
+        pack();
+
+        setTitle(titulo);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Evento producido por " + e.getActionCommand());
+    }
+
+    public static void main (String[] args) {
+        WindowListener l = new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+
+        JFrame frame = new AgruparBotones("Curso de Java: Agrupar Botones");
+        frame.addWindowListener(l);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
